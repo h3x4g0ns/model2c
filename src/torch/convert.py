1fc61264b38f7da0cac7eaf78b73a7a8bb74dbc8
Ekansh Agrawal <agrawalekansh02@gmil.com>
2022-07-08 14:37:06 -0700

    created examples

--- src/torch/convert.py
+++ src/torch/convert.py
@@ -6,4 +6,13 @@ def convert(model):
 
     .py -> torch -> onnx -> .c
     """
-    pass
+    # Convert to onnx
+    onnx_model = torch.onnx.export(model, model.input, "model.onnx")
+
+    # Convert to c
+    c_model = torch.onnx.export_to_c(onnx_model, "model.c")
+
+    # Save c model
+    with open("model.c", "w") as f:
+        f.write(c_model)
+    print("Model saved to model.c")
