6b226d828452d03ea3067e605816e0e3e02c8d6f
Ekansh Agrawal <agrawalekansh02@gmil.com>
2022-07-08 16:17:52 -0700

    init test success

--- src/model2c/pytorch/pytorch.py
+++ src/model2c/pytorch/pytorch.py
@@ -1,4 +1,4 @@
-import torch.onnx
+import torch, torch.onnx
 import os
 
 def convert(model, input_size):
@@ -7,9 +7,11 @@ def convert(model, input_size):
 
     .py -> torch -> onnx -> .c
     """
+    inputs = torch.randn(input_size)
+
     # convert to onnx
     torch.onnx.export(model,                 # model being run
-                  model.inputs,              # model input (or a tuple for multiple inputs)
+                  inputs,              # model input (or a tuple for multiple inputs)
                   "model.onnx",              # where to save the model (can be a file or file-like object)
                   export_params=True,        # store the trained parameter weights inside the model file
                   opset_version=10,          # the ONNX version to export the model to
@@ -19,4 +21,4 @@ def convert(model, input_size):
 
 
     # convert to c
-    os.system("onnx2c model.onnx model.c") 
+    os.system("onnx2c model.onnx > model.c") 
